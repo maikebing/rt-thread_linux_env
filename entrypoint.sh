acf9ff9198214b784c58b235a805611c9f33c814
Yanhong.Ma <yanhong.ma@xr-techs.com>
2022-07-28 14:02:41 +0800

    修改

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 if [ ! -d ~/.env ]; then ln  /env ~/.env  -s;fi
 export PATH=$PATH:~/.env/tools/scripts
 sed -i -e 's/CONFIG_SYS_PKGS_DOWNLOAD_ACCELERATE=y/CONFIG_SYS_PKGS_DOWNLOAD_ACCELERATE=n/g'  /env/tools/scripts/cmds/.config
-TARGET=$1
-STD=$2
 pkgs --printenv
 pkgs  --list
 pkgs --update
-cppcheck --enable=all --std=$STD  applications/
-scons --target=$TARGET 
+cppcheck --enable=all --std=${STD} applications/
+scons --target=${TARGET}
