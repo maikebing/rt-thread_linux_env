fa20ab7e8a8287256703f002c6eb75167b274450
Yanhong.Ma <yanhong.ma@xr-techs.com>
2022-07-28 13:50:47 +0800

    修改action描述

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,10 @@
 if [ ! -d ~/.env ]; then ln  /env ~/.env  -s;fi
 export PATH=$PATH:~/.env/tools/scripts
 sed -i -e 's/CONFIG_SYS_PKGS_DOWNLOAD_ACCELERATE=y/CONFIG_SYS_PKGS_DOWNLOAD_ACCELERATE=n/g'  /env/tools/scripts/cmds/.config
+TARGET=$1
+STD=$2
+pkgs --printenv
+pkgs  --list
+pkgs --update
+cppcheck --enable=all --std=$STD  applications/
+scons --target=$TARGET 
