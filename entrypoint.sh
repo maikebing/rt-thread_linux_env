479923cfd19bc73d5cf5f4de7f0eae84d4672974
Yanhong.Ma <yanhong.ma@xr-techs.com>
2022-07-28 14:15:46 +0800

    xiugai

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
-if [ ! -d ~/.env ]; then ln  /env ~/.env  -s;fi
+if [ ! -d ~/.env ]; then
+ ln  /env ~/.env  -s
+ fi
 export PATH=$PATH:~/.env/tools/scripts
 sed -i -e 's/CONFIG_SYS_PKGS_DOWNLOAD_ACCELERATE=y/CONFIG_SYS_PKGS_DOWNLOAD_ACCELERATE=n/g'  /env/tools/scripts/cmds/.config
-pkgs --printenv
-pkgs  --list
-pkgs --update
-cppcheck --enable=all --std=${STD} applications/
-scons --target=${TARGET}
+
+if [-f SConstruct]; then
+    pkgs --printenv
+    pkgs  --list
+    pkgs --update
+    if [[ -z "${CPPCHECK}" ]]; then
+    cppcheck ${CPPCHECK} 
+    fi
+
+    
+    if [[ -z "${TARGET}" ]]; then
+    scons --target=${TARGET}
+    else
+    scons 
+    fi
+else
+echo "fogret checkout?"    
+fi    
